2da4b3da3bf1db8120bf3ddb65d8da4118e0b1e6
HungDoan <doananhlehung@gmail.com>
2025-04-02 02:03:05 +0700

    update swagger + restruct folder HMVC modules

--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,15 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $modulePaths = app_path('Modules');
+        
+        if(file_exists($modulePaths)) {
+            $modules = scandir($modulePaths);
+            foreach($modules as $module) {
+                if(is_dir($modulePaths . '/' . $module) && $module !== '.' && $module !== '..') {
+                    View::addNamespace($module, $modulePaths . '/' . $module . '/Views');
+                }
+            }
+        }
     }
 }
